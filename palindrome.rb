7d9b721372df38262bbb08da8f31c8015c4814d9
Ryan Poplawski <claydewd@gmail.com>
2015-09-10 10:02:50 -0500

    trying palindrome tdd

---
+++ palindrome.rb
@@ -0,0 +1,52 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+# Write a method which accepts one parameter.  The method should return true if
+# the string passed to it is a palindrome.  It should return false if the string
+# is not a palindrome
+
+# WRITE YOUR CODE BELOW HERE.
+
+# WRITE YOUR CODE ABOVE HERE.
+def palindrome?(word)
+  adjusted_word == word.gsub(/[ ,\.!'’ ;]/, '’).downcase
+
+  adjusted_word == cleaned_word.reverse
+end
+
+class StringPalindromeChallenge < MiniTest::Test
+  def test_words
+    assert palindrome?("tacocat")
+    assert palindrome?("anna")
+    assert palindrome?("racecar")
+    refute palindrome?("ruby")
+    refute palindrome?("cowboy")
+  end
+
+  def test_capitalization
+    assert palindrome?("Tacocat")
+    assert palindrome?("Anna")
+    assert palindrome?("RacEcAr")
+    refute palindrome?("Ruby")
+    refute palindrome?("Cowboy")
+  end
+
+  def test_sentences
+    assert palindrome?("Stressed desserts")
+    assert palindrome?("Stop on no pots")
+    refute palindrome?("The quick brown fox")
+  end
+
+  def test_spaces
+    assert palindrome?("Mad as Adam")
+    assert palindrome?("Sums are not set as a test on Erasmus")
+    refute palindrome?("Where the heck did you get these sentences?")
+  end
+
+  def test_punctuation
+    assert palindrome?("A car, a man, a maraca.")
+    assert palindrome?("Satan, oscillate my metallic sonatas!")
+    assert palindrome?("Pull up; Eva, we’ re here! Wave! Pull up!")
+    refute palindrome?("Wait! Don't drive off that cliff!")
+  end
+end
